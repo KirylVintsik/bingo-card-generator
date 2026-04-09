1acb319bbcb290f0d7cf84aad768491d769494c2
Kiryl Vintsik <vintik.kirill@gmail.com>
2026-04-09 22:30:55 +0200

    fixing ts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,8 +1,11 @@
 import express from 'express';
+import { connectDB } from './config/database';
+
 const app = express();
 
 const port = 3000;
 
+connectDB();
 app.get('/', (req, res) => {
   res.send('Hello Deez!');
 });
